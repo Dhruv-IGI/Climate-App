08c41b886c71b3e260f99fe1d81c7a1e557b88d1
Dhruv varshney <83370198+Dhruv-IGI@users.noreply.github.com>
2022-03-04 20:48:40 +0530

    Update location.dart

--- lib/location.dart
+++ lib/location.dart
@@ -5,12 +5,10 @@ class Location {
 
   Future getCurrentLocation() async {
     try {
-      // Position position = await Geolocator()
-      //     .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      // this.latitude = position.latitude;
-      // this.longitude = position.longitude;
-      // this.latitude = 30.46453533497689;
-      // this.longitude = 78.05643754257252;
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      this.latitude = position.latitude;
+      this.longitude = position.longitude;
       this.latitude = 28.680871;
       this.longitude = 77.298668;
     } catch (e) {
@@ -19,8 +17,3 @@ class Location {
   }
 }
 
-// String cityName = decodedData['location']['name'];
-
-// int condition = decodedData['current']['condition']['code'];
-
-// double temperature = decodedData['current']['temp_c'];
